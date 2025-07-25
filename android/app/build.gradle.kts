4eb95f244a9499adabb3c02e4d28017834db0866
osuyaalex <mac@MacBook-Pro.local>
2025-07-25 21:13:34 +0100

    new

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,18 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+
+    flavorDimensions += "env"
+    productFlavors {
+        create("dev") {
+            dimension = "env"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+        }
+        create("prod") {
+            dimension = "env"
+        }
+    }
 }
 
 flutter {
